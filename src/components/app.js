a297f00b47e4431e1092f388c85b62de0461e2c1
firecoded <yepyepguru@gmail.com>
2018-08-31 15:03:12 -0700

    auth hoc completed

--- src/components/app.js
+++ src/components/app.js
@@ -9,6 +9,7 @@ import About from './about';
 import SecretList from './secret_list';
 import MovieQuote from './movie_quotes.js';
 import SignUp from './sign_up';
+import auth from '../hoc/auth';
 
 const App = () => (
     <div>
@@ -16,7 +17,7 @@ const App = () => (
     	<div className = 'container'>
     		<Route exact path ='/' component = {Home}/>
     		<Route path = '/about' component = {About}/>
-    		<Route path = '/secret-list' component = {SecretList}/>
+    		<Route path = '/secret-list' component = {auth(SecretList)}/>
     		<Route path = '/movie-quote' component = {MovieQuote}/>
     		<Route path = '/sign-up' component = {SignUp}/>
     	</div>
